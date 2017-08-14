cba35074bb6919965752ecda6e4db7f3ad4a9963
unicorn808 <unicorncalcs@gmail.com>
2017-08-13 14:07:41 -1000

    just updating things

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-C:\Users\hocke\Downloads\electron-v1.6.12-win32-x64\electron.exe C:\Users\hocke\OneDrive\Documents\Electron\bbc-converter
+C:\Users\hocke\Downloads\electron-v1.6.12-win32-x64\electron.exe %cd%
 PAUSE
